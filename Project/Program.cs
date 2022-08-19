0991c6ed4bfb470d5b3289c98273a64dadb750b6
Vadim Bayramov <bayramovva@gmail.com>
2022-08-20 01:55:30 +0300

    Add Zadacha 56

--- Project/Program.cs
+++ Project/Program.cs
@@ -22,9 +22,42 @@
         {
             //Задайте прямоугольный двумерный массив. Напишите программу,
             //которая будет находить строку с наименьшей суммой элементов.
+            
+            Random random = new Random();
+            int rows = random.Next(6, 7);
+            int columns = random.Next(2, 4);
+            int[,] array = new int[rows, columns];
+            FillArray(array, 0, 9);
+            PrintArray(array);
+            Console.WriteLine();
+
+          
+            
+            for (int i = 0; i < rows; i++)
+            {
+                int sumRow = 0;
+                int nextRow = 0;
+                int MinRow = i; 
+
+                for (int j = 0; j < columns; j++)
+                {
+                    sumRow += array[i, j];
+                    Console.WriteLine($"sumRow {sumRow}");
+                    nextRow += array[i+1, j];
+                    Console.WriteLine($"nextRow {nextRow}");
+                }
+
+            if (sumRow > nextRow)
+            {
+                MinRow = i+1;
+            }
+            Console.WriteLine(MinRow);
+            }
+
         }
 
-        Zadacha54();
+        //Zadacha54();
+        Zadacha56();
     }
     static void FillArray(int[,] array, int startNum = 0, int finishNum = 10)
     {
